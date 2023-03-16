4f91c74daefa1101b39cea8d4f1f8b14771cac87
sinomau <sinopolimauro@gmail.com>
2023-03-15 22:36:58 -0300

    liveMatch

--- js/api.js
+++ js/api.js
@@ -5,11 +5,13 @@ const infoApiContainer = document.querySelector(".info-api-container");
 const buttonStandings = document.querySelector(".table-api");
 const buttonNextGames = document.querySelector(".next-api");
 const buttonNowGames = document.querySelector(".now-api");
+const buttonLiveGames = document.querySelector(".live-api");
 
 /* Event handlers */
 buttonNowGames.addEventListener("click", getGamesToday);
 buttonStandings.addEventListener("click", tablePositions);
 buttonNextGames.addEventListener("click", nextGames);
+buttonLiveGames.addEventListener("click", getGameLive);
 
 async function tablePositions() {
   try {
@@ -208,7 +210,7 @@ function renderTodayGames() {
   const data = JSON.parse(sessionStorage.getItem("gamesToday"));
   if (data.length === 0) {
     infoApiContainer.innerHTML = `
-    <h1>No hay partidos definidos para el dia de hoy</h1>
+    <h1>No hay partidos definidos para el día de hoy</h1>
     `;
     infoApiContainer.setAttribute("aria-busy", "false");
   } else {
@@ -239,6 +241,109 @@ function renderTodayGames() {
   <p><img src="${awayLogo}"class="local-logo">${partidos.teams.away.name} -  ${goalAway}</p>
   </div>
   <p>Estado de partido: ${partidos.fixture.status.elapsed} Min  ${partidos.fixture.status.long}</p>
+  <p>Horario : ${dateArg}</p>
+  <p>Arbitraje: ${partidos.fixture.referee}</p>
+  <p>Estadio : ${partidos.fixture.venue.name} -- ${partidos.fixture.venue.city}</p>
+  </div>
+  </article>
+
+  `;
+      infoApiContainer.setAttribute("aria-busy", "false");
+    });
+  }
+}
+
+async function getGameLive() {
+  try {
+    infoApiContainer.setAttribute("aria-busy", "true");
+    const gameLive = sessionStorage.getItem("gameLive");
+    if (gameLive != null) {
+      renderGameLive();
+    } else {
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, "0");
+      const day = String(date.getDate()).padStart(2, "0");
+      const formattedDate = `${year}-${month}-${day}`;
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "ea8ec82cebb59731159876110f477704",
+          "X-RapidAPI-Host": "api-football-v1.p.rapidapi.com",
+        },
+      };
+
+      const response = await fetch(
+        `https://v3.football.api-sports.io/fixtures?&live=128-129-130-131-132-133-134-517&timezone=America/Argentina/Buenos_Aires`,
+        options
+      );
+      const data = await response.json();
+      const resp = data.response;
+      sessionStorage.setItem("gameLive", JSON.stringify(resp));
+      console.log(resp);
+
+      renderGameLive();
+    }
+  } catch (error) {
+    console.log(error);
+    infoApiContainer.innerHTML = `
+    <h1>Contenido no disponible intente mas tarde</h1>
+    `;
+  }
+}
+
+function renderGameLive() {
+  const data = JSON.parse(sessionStorage.getItem("gameLive"));
+  if (data.length === 0) {
+    infoApiContainer.innerHTML = `
+    <h1>No hay partidos en vivo en este momento</h1>
+    `;
+    infoApiContainer.setAttribute("aria-busy", "false");
+  } else {
+    console.log(data);
+    infoApiContainer.innerHTML = "";
+    data.forEach((fixture) => {
+      const gameTime = fixture.league.round;
+    });
+
+    data.forEach((partidos) => {
+      const dateGame = new Date(partidos.fixture.date);
+      const dateArg = dateGame.toLocaleString("en-GB");
+      const homeLogo = partidos.teams.home.logo;
+      const awayLogo = partidos.teams.away.logo;
+
+      let goalHome = partidos.goals.home;
+      let goalAway = partidos.goals.away;
+      if (goalAway === null || goalHome === null) {
+        goalAway = 0;
+        goalHome = 0;
+      }
+
+      if (partidos.fixture.referee === null) {
+        partidos.fixture.referee = "Sin Informacion";
+      }
+
+      if (partidos.fixture.status.long === "Second Half") {
+        partidos.fixture.status.long = "Segundo Tiempo";
+      } else if (partidos.fixture.status.long === "First Half") {
+        partidos.fixture.status.long = "Primer Tiempo";
+      } else if (partidos.fixture.status.long === "Halftime") {
+        partidos.fixture.status.long = "Entretiempo";
+      } else if (partidos.fixture.status.long === "Match Finished") {
+        partidos.fixture.status.long = "Terminado";
+      }
+
+      infoApiContainer.innerHTML += ` 
+  <article class="article-container">
+  <div class="info-container">
+  <p>Partido de la fecha : ${partidos.league.round}</p>
+  <p>Torneo: ${partidos.league.name}</p>
+  <div class="teams-container">
+  <p><img src="${homeLogo}"class="local-logo">${partidos.teams.home.name} -  ${goalHome}</p>
+  <p><img src="${awayLogo}"class="local-logo">${partidos.teams.away.name} -  ${goalAway}</p>
+  </div>
+  <p>Tiempo Trasncurrido: ${partidos.fixture.status.elapsed} Min  </p>
+  <p>Estado de partido: ${partidos.fixture.status.long}</p>
   <p>Horario : ${dateArg}</p>
   <p>Arbitraje: ${partidos.fixture.referee}</p>
   <p>Estadio : ${partidos.fixture.venue.name} -- ${partidos.fixture.venue.city}</p>
