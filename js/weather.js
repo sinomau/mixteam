0bce591dd3ed431c39367c2268166033cd76dd05
sinomau <sinopolimauro@gmail.com>
2023-04-09 15:24:53 -0300

    newsAdd

--- js/weather.js
+++ js/weather.js
@@ -39,6 +39,9 @@ const renderData = async (response) => {
     const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
     const weatherDescription = description.slice(0, 200) + "...";
     const uppercase = description.toUpperCase();
+    if (city === "San Nicolas") {
+      city = "Buenos Aires ";
+    }
 
     document.querySelector(".weather-container").innerHTML = `
         
@@ -56,4 +59,84 @@ const renderData = async (response) => {
     <h1>Contenido no disponible intente mas tarde</h1>
     `;
   }
+};
+
+const apiNews = async () => {
+  try {
+    const newsContainer = document.querySelector(".news-container");
+    const allNewsContainer = document.querySelector(".all-news-container");
+    const apikey = "41dca1972cc7453e9401a408e94faae4";
+    const country = "ar";
+    const category = "sports";
+
+    const news = await fetch(
+      `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apikey}`
+    );
+
+    const response = await news.json();
+    console.log(response);
+    renderNews(response, newsContainer);
+    renderAllNews(response, allNewsContainer);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+apiNews();
+
+const renderNews = async (response, newsContainer) => {
+  try {
+    console.log(response);
+    let news = response.articles;
+
+    let count = 0;
+    news.forEach((article) => {
+      if (count >= 2) {
+        return; // si ya se han agregado 2 noticias, detener la iteración
+      }
+
+      let { urlToImage, title, description, url } = article;
+      if (description === null) {
+        description = "";
+      }
+
+      newsContainer.innerHTML += `
+        <article class="card-news">
+          <h6>${title}</h6>
+          <p>${description}</p>
+          <a href="${url}" target="_blank">Ver más</a>
+        </article>
+      `;
+
+      count++;
+    });
+    let = cardNews = document.querySelector(".card-news");
+    newsContainer.innerHTML += `
+    <a href="./pages/news.html">Ver más noticias</a>
+  `;
+    newsContainer.setAttribute("aria-busy", "false");
+  } catch (err) {
+    console.log(err);
+    newsContainer.innerHTML = `
+      <h1>Contenido no disponible, intente más tarde</h1>
+    `;
+  }
+};
+
+const renderAllNews = async (response, allNewsContainer) => {
+  const news = response.articles;
+  news.forEach((article) => {
+    let { urlToImage, title, description, url } = article;
+    if (description === null) {
+      description = "";
+    }
+
+    allNewsContainer.innerHTML += `
+    <article class="card-all-news">
+      <h6>${title}</h6>
+      <p>${description}</p>
+      <a href="${url}" target="_blank">Ver más</a>
+    </article>
+  `;
+  });
 };
